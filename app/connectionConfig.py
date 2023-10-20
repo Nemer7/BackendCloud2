93b5c165a53b4be6a14381c9fea0bffd929873c8
Nemer7 <nvelandia@unicesar.edu.co>
2023-10-20 17:44:26 -0500

    bd

--- app/connectionConfig.py
+++ app/connectionConfig.py
@@ -1,25 +1,31 @@
-import psycopg2
 import sys
 import boto3
 import os
+import mysql.connector
 
-ENDPOINT="postgresmydb.123456789012.us-east-1.rds.amazonaws.com"
-PORT="5432"
-USER="jane_doe"
-REGION="us-west-2"
-DBNAME="mydb"
+aws_access_key = "AKIAXGYRZECKWFDZH56L"
+aws_secret_key = "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"
+aws_region = "us-east-2"  # Por ejemplo, "us-east-1"
+db_instance_identifier = "rds-lab"
 
-#gets the credentials from .aws/credentials
-session = boto3.Session(profile_name='RDSCreds')
-client = session.client('rds')
 
-token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
+client = boto3.client("rds", region_name=aws_region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
 
-try:
-    conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=token, sslrootcert="SSLCERTIFICATE")
-    cur = conn.cursor()
-    cur.execute("""SELECT now()""")
-    query_results = cur.fetchall()
-    print(query_results)
-except Exception as e:
-    print("Database connection failed due to {}".format(e))
+response = client.describe_db_instances(DBInstanceIdentifier=db_instance_identifier)
+endpoint = response["DBInstances"][0]["Endpoint"]["Address"]
+username = "admin"
+password = "Chepe1224."
+
+
+def connect():
+    try: 
+        connection = mysql.connector.connect(
+        host=endpoint,
+        user=username,
+        password=password,
+        database="filesdb"
+)
+
+        print("Conexion exitosa")
+    except Exception as e:
+        print("Database connection failed due to {}".format(e))
